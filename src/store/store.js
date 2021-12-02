57a8ae35fb9d5a44b4fb4ec192e9bcd1bd2cac57
Abhay5855 <ap3919529@gmail.com>
2021-12-02 18:19:21 +0530

    created a simple reducer with action types and Actions and how getState and dispatch works

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,19 @@
 import { createStore } from "redux";
-import Simplereducer from "../redux/reducers/Simplereducer";
+import Simplereducer, { increment } from "../redux/reducers/Simplereducer";
 
 
  const store = createStore(Simplereducer);
 
+
+
+ store.dispatch(increment());
+
  console.log(store);
+
+ console.log(store.dispatch(increment()));
+
+ console.log(store.getState());
+
 
 
  export default store;
