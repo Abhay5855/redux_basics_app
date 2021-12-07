26f96bb568b129faba61f0562a22b9f9509239b0
Abhay5855 <ap3919529@gmail.com>
2021-12-08 00:43:38 +0530

    Creating two components fro displaying the characters and showing the selected character

---
+++ src/components/rick_morty_components/Display_Characters.js
@@ -0,0 +1,15 @@
+
+
+
+const Display_Characters = () => {
+
+     return (
+          <>
+
+          <h2>Rick and Morty characters</h2>
+          
+          </>
+     )
+}
+
+export default Display_Characters;
---
+++ src/components/rick_morty_components/Selected_Character.js
@@ -0,0 +1,16 @@
+
+
+
+
+const Selected_Characters = () => {
+
+    return (
+         <>
+
+         <h2>Rick and Morty characters</h2>
+         
+         </>
+    )
+}
+
+export default Selected_Characters;
